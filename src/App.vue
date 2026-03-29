7dcdd9c07424b16f7ca576a5e3056d515e9999f1
wiemab11 <166920302+wiemab11@users.noreply.github.com>
2026-03-30 00:29:06 +0100

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Navbar />
 </template>
 
-<style scoped></style>
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <h1>Bookstore</h1>
+    <div class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(to right, #2c3e50, #3498db);
+  color: white;
+  padding: 15px 30px;
+}
+
+.links a {
+  margin-left: 20px;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+</style>
